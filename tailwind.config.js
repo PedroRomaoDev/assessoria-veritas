15087b7c098fbf997a59f1a9ca8650b22c0f05dd
PedroRomaoDev <pedro7ntj@gmail.com>
2026-01-08 20:38:04 -0300

    chore: configure Tailwind CSS and PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./main.jsx",
+    "./pages/**/*.{js,jsx}",
+    "./Componentes/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
