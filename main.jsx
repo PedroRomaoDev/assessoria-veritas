6fa658a6a5ead95d1de2af576c5a6b5796d10ca1
PedroRomaoDev <pedro7ntj@gmail.com>
2026-01-08 20:35:35 -0300

    feat: add core application setup with Vite and Tailwind CSS

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Lar from "./pages/Lar.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Lar />
+  </React.StrictMode>
+);
